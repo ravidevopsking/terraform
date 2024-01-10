29c114ff06fa9427622ecba0bafe9b3dd2b26c39
Ravi Teja <122197318+ravidevopsking@users.noreply.github.com>
2024-01-10 19:26:01 +0530

    provider file

--- MODULES/project-ec2/outputs.tf
+++ MODULES/project-ec2/outputs.tf
@@ -1,4 +1,12 @@
 output "total_output" {
-  value = module.project-ec2.total_output.metadata_options
-  #from here i am fetching metadata from total output
+  value = module.project-ec2.total_output.metadata_options["0"].http_tokens
+  #from here i am fetching last data from total output metadata_options
+}
+
+output "public" {
+  value = module.project-ec2.public_ip
+}
+
+output "private" {
+  value = module.project-ec2.private_ip
 }
