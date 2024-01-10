093a528c76316ee0247ca0dd18fe9d51aeb4e631
Ravi Teja <122197318+ravidevopsking@users.noreply.github.com>
2024-01-10 19:16:25 +0530

    outputs tested

--- MODULES/ec2/outputs.tf
+++ MODULES/ec2/outputs.tf
@@ -1,10 +1,11 @@
 output "total_output" {
     value = aws_instance.ec2
- 
+    #fetches all the information
 }
 
 output "public_ip" {
   value = aws_instance.ec2.public_ip
+  #fetches only public ip
 }
 
 output "private_ip" {
---
+++ MODULES/project-ec2/outputs.tf
@@ -0,0 +1,4 @@
+output "total_output" {
+  value = module.project-ec2.total_output.metadata_options
+  #from here i am fetching metadata from total output
+}
