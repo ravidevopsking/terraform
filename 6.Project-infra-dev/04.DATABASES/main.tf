56bb5c906b39fcc6a2225212574e255a007ca284
Ravi Teja <122197318+ravidevopsking@users.noreply.github.com>
2024-01-24 12:36:56 +0530

    updated

--- 6.Project-infra-dev/04.DATABASES/main.tf
+++ 6.Project-infra-dev/04.DATABASES/main.tf
@@ -1,5 +1,6 @@
+#using below module mongodb ec2 instance is created, but not configured.
 module "mongodb" {
-  source                 = "terraform-aws-modules/ec2-instance/aws"
+  source                 = "terraform-aws-modules/ec2-instance/aws"  #module from terraform website
   ami = data.aws_ami.centos8.id
   name                   = "${local.ec2_name}-mongodb"
   instance_type          = "t3.small"
@@ -16,8 +17,11 @@ module "mongodb" {
   )
 }
 
+#using below module we are configuring mongodb server using bootstrap script in which ansible execute tasks on mongodb server.
+#using null resource we are connecting to mongodb server through ssh and using provisioners to run bootstrap or shell script to achieve desired configurations on mongodb server. Bootstrap script has ansible which performs tasks on server to get mongodb configurations on it.
 resource "null_resource" "mongodb" {
   # Changes to any instance of the cluster requires re-provisioning
+  #it triggeres if there is any change to mongodb
   triggers = {
     instance_id = module.mongodb.id
   }
@@ -25,12 +29,12 @@ resource "null_resource" "mongodb" {
   # Bootstrap script can run on any instance of the cluster
   # So we just choose the first in this case
   connection {
-    host = module.mongodb.private_ip
+    host = module.mongodb.private_ip                #mongodb doesnt have public ip as it is in private subnet
     type = "ssh"
     user = "centos"
     password = "DevOps321"
   }
-
+  #copying file from local to remote machine using provisioner file
   provisioner "file" {
     source      = "bootstrap.sh"
     destination = "/tmp/bootstrap.sh"
@@ -39,8 +43,8 @@ resource "null_resource" "mongodb" {
   provisioner "remote-exec" {
     # Bootstrap script called with private_ip of each node in the cluster
     inline = [
-      "chmod +x /tmp/bootstrap.sh",
-      "sudo sh /tmp/bootstrap.sh mongodb dev"
+      "chmod +x /tmp/bootstrap.sh",             #giving execution permissions to script
+      "sudo sh /tmp/bootstrap.sh mongodb dev"   #executing bootstrap script
     ]
   }
 }
@@ -186,4 +190,46 @@ resource "null_resource" "rabbitmq" {
       "sudo sh /tmp/bootstrap.sh rabbitmq dev"
     ]
   }
+}
+
+#below module is used to create records for all databases
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+
+  zone_name = var.zone_name
+
+  records = [
+    {
+      name    = "mongodb-dev"
+      type    = "A"
+      ttl     = 1
+      records = [
+        module.mongodb.private_ip,
+      ]
+    },
+    {
+      name    = "redis-dev"
+      type    = "A"
+      ttl     = 1
+      records = [
+        module.redis.private_ip,
+      ]
+    },
+    {
+      name    = "mysql-dev"
+      type    = "A"
+      ttl     = 1
+      records = [
+        module.mysql.private_ip,
+      ]
+    },
+    {
+      name    = "rabbitmq-dev"
+      type    = "A"
+      ttl     = 1
+      records = [
+        module.rabbitmq.private_ip,
+      ]
+    },
+  ]
 }
--- 6.Project-infra-dev/README.md
+++ 6.Project-infra-dev/README.md
@@ -1,1 +1,5 @@
 
+Here ALL databases instances are first created using module
+ALL databases(mongodb,mysql,redis,rabbitmq) are configured using null resource provisioners.
+using provisioners we executed bootstrap script on target host
+bootstap script has ansible commands to execute its tasks from ansible roles on target server to achieve desired configurations.
