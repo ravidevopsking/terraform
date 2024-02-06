dbe4e04b0aee0b25e709c1316a072c390a5be58f
Ravi Teja <122197318+ravidevopsking@users.noreply.github.com>
2024-02-06 22:20:07 +0530

    updated

--- 6.Project-infra-dev/13.cart/main.tf
+++ 6.Project-infra-dev/13.cart/main.tf
@@ -1,8 +1,7 @@
-module "shipping" {
+module "cart" {
   source = "../../terraform-roboshop-app"
-  #source = "git::https://github.com/ravidevopsking/terraform-roboshop-app.git?ref=main"
   vpc_id = data.aws_ssm_parameter.vpc_id.value
-  component_sg_id = data.aws_ssm_parameter.shipping_sg_id.value
+  component_sg_id = data.aws_ssm_parameter.cart_sg_id.value
   private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value) #list of private subnte_ids
   iam_instance_profile = var.iam_instance_profile
   project_name = var.project_name
@@ -11,6 +10,6 @@ module "shipping" {
   tags = var.tags
   zone_name = var.zone_name
   app_alb_listener_arn = data.aws_ssm_parameter.app_alb_listener_arn.value
-  rule_priority = 30
+  rule_priority = 40
 }
 
